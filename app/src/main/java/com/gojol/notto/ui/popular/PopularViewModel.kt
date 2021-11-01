9a10f253bbeec99d9cbfdac7d3fc26ec29b6bfcb
Hyesung <comet_0419@naver.com>
2021-11-01 17:50:42 +0900

    #2 [feat] Bottom Navigation 이름 변경

--- app/src/main/java/com/gojol/notto/ui/popular/PopularViewModel.kt
+++ app/src/main/java/com/gojol/notto/ui/popular/PopularViewModel.kt
@@ -1,13 +1,13 @@
-package com.gojol.notto.ui.notifications
+package com.gojol.notto.ui.popular
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class PopularViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is notifications Fragment"
+        value = "This is dashboard Fragment"
     }
     val text: LiveData<String> = _text
 }
