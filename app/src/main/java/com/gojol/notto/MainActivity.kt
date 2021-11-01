4a3f00a2a282ecaaad502095ba4b534657f76478
Hyesung82 <khs990419@gmail.com>
2021-11-01 18:58:29 +0900

    [fix] commit 내역 revert

--- app/src/main/java/com/gojol/notto/MainActivity.kt
+++ app/src/main/java/com/gojol/notto/MainActivity.kt
@@ -26,7 +26,7 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home, R.id.navigation_popular, R.id.navigation_option
+                R.id.navigation_home, R.id.navigation_dashboard, R.id.navigation_notifications
             )
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
