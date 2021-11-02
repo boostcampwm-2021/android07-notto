808e86b825f6b55503c229532241b95445dd8e92
dddooo9 <dlaehdus99@naver.com>
2021-11-02 17:23:05 +0900

    #8 #9 [fix] Todo, Label 패키지 이동 및 프로퍼티 변경

--- app/src/main/java/com/gojol/notto/model/database/todo/Todo.kt
+++ app/src/main/java/com/gojol/notto/model/database/todo/Todo.kt
@@ -1,13 +1,11 @@
-package com.gojol.notto.model.data
+package com.gojol.notto.model.database.todo
 
-import androidx.room.ColumnInfo
-import androidx.room.Entity
-import androidx.room.PrimaryKey
+import androidx.room.*
+import com.gojol.notto.model.data.RepeatType
 
 @Entity
 data class Todo(
     @ColumnInfo(name = "is_success") val isSuccess: Boolean,
-    @ColumnInfo(name = "label_list") val labelList: List<Label>,
     @ColumnInfo(name = "content") val content: String,
     @ColumnInfo(name = "target_date") val targetDate: String,
     @ColumnInfo(name = "is_repeat") val isRepeat: Boolean,
@@ -16,8 +14,6 @@ data class Todo(
     @ColumnInfo(name = "start_time") val startTime: String,
     @ColumnInfo(name = "end_time") val endTime: String,
     @ColumnInfo(name = "period_time") val periodTime: String,
-    @ColumnInfo(name = "is_keyword_open") val isKeywordOpen: Boolean
-) {
-    @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id") var id: Int = 0
-}
+    @ColumnInfo(name = "is_keyword_open") val isKeywordOpen: Boolean,
+    @PrimaryKey(autoGenerate = true) var todoId: Int = 0
+)
