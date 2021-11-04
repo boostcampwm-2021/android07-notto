5a709f60206be6db549c484e37b3a38971e5ab72
keykat <koin12000@naver.com>
2021-11-04 16:45:18 +0900

    [feat] viewmodel 수정

--- app/src/main/java/com/gojol/notto/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/gojol/notto/ui/home/HomeViewModel.kt
@@ -3,19 +3,21 @@ package com.gojol.notto.ui.home
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.gojol.notto.common.TodoSuccessType
 import com.gojol.notto.model.data.BindingData
 import com.gojol.notto.model.database.label.Label
 import com.gojol.notto.model.data.RepeatType
-import com.gojol.notto.model.database.label.Label
 import com.gojol.notto.model.database.todo.Todo
 import com.gojol.notto.model.datasource.todo.TodoLabelRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(private val repository: TodoLabelRepository) : ViewModel() {
 
-    private val dummyLabels = listOf(
+    private val dummyLabels = mutableListOf(
         Label(0, "학교"),
         Label(1, "동아리"),
         Label(2, "집"),
@@ -31,25 +33,50 @@ class HomeViewModel @Inject constructor(private val repository: TodoLabelReposit
         Label(6, "주말"),
     )
 
-    private val dummyTodos = listOf(
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
-        Todo(false, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+    private val dummyTodos = mutableListOf(
+        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+//        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+//        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+//        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
+//        Todo(TodoSuccessType.NOTHING, "안녕하세요", "1", false, RepeatType.DAY, false, "1:00", "2:00", "1:00", false),
     )
 
-    private val _date = MutableLiveData<String>("2021년 11월")
+    private val _date = MutableLiveData("2021년 11월")
     val date: LiveData<String> = _date
 
-    private val _labelList = MutableLiveData<List<Label>>(dummyLabels)
-    val labelList: LiveData<List<Label>> = _labelList
+    private val _labelList = MutableLiveData<MutableList<Label>>()
+    val labelList: LiveData<MutableList<Label>> = _labelList
 
-    private val _todoList = MutableLiveData<List<Todo>>(dummyTodos)
-    val todoList: LiveData<List<Todo>> = _todoList
+    private val _todoList = MutableLiveData<MutableList<Todo>>()
+    val todoList: LiveData<MutableList<Todo>> = _todoList
 
-    private val _concatList = MutableLiveData(BindingData(todoList.value, labelList.value))
+    private val _concatList = MutableLiveData<BindingData>()
     val concatList: LiveData<BindingData> = _concatList
+
+    fun insertTodos() {
+        viewModelScope.launch {
+            dummyTodos.forEach {
+                repository.insertTodo(it)
+            }
+            _todoList.value = repository.getAllTodo().toMutableList()
+        }
+    }
+
+    fun insertLabels() {
+        viewModelScope.launch {
+            dummyLabels.forEach {
+                repository.insertLabel(it)
+            }
+            _labelList.value = repository.getAllLabel().toMutableList()
+        }
+    }
+
+    fun fetchTodoSuccessState(todo: Todo) {
+        viewModelScope.launch {
+            repository.updateTodo(todo)
+            _todoList.value = repository.getAllTodo().toMutableList()
+        }
+    }
 }
