f0092426859b8cff09db037c7d4c85b94474eb05
Hyesung <comet_0419@naver.com>
2021-11-04 21:11:04 +0900

    #56 [feat] SplashActivity.kt 추가

---
+++ app/src/main/java/com/gojol/notto/ui/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.gojol.notto.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gojol.notto.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
