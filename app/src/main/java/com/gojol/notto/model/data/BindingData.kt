03fe620c79bf0d46e57e68f1dd6798df779d44a7
keykat <koin12000@naver.com>
2021-11-04 17:05:30 +0900

    [fix] BindingData, BindingAdapter - LabelWithCheck로 수정

--- app/src/main/java/com/gojol/notto/model/data/BindingData.kt
+++ app/src/main/java/com/gojol/notto/model/data/BindingData.kt
@@ -2,8 +2,9 @@ package com.gojol.notto.model.data
 
 import com.gojol.notto.model.database.label.Label
 import com.gojol.notto.model.database.todo.Todo
+import com.gojol.notto.ui.home.LabelWithCheck
 
 data class BindingData(
     val todoList: List<Todo>?,
-    val labelList: List<Label>?
+    val labelList: List<LabelWithCheck>?
 )
--- app/src/main/java/com/gojol/notto/ui/home/BindAdapter.kt
+++ app/src/main/java/com/gojol/notto/ui/home/BindAdapter.kt
@@ -5,22 +5,20 @@ import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.ConcatAdapter
 import androidx.recyclerview.widget.RecyclerView
 import com.gojol.notto.model.data.BindingData
-import com.gojol.notto.model.database.label.Label
-import com.gojol.notto.model.database.todo.Todo
 
 @BindingAdapter("item")
-fun bindItems(recyclerview: RecyclerView, concatList: BindingData) {
+fun bindItems(recyclerview: RecyclerView, concatList: BindingData?) {
     val adapter = recyclerview.adapter as ConcatAdapter
     adapter.adapters.forEach {
         Log.d("adapter", it.toString())
         when(it) {
             is TodoAdapter -> {
-                concatList.todoList?.let { list ->
+                concatList?.todoList?.let { list ->
                     it.submitList(list)
                 }
             }
             is LabelWrapperAdapter -> {
-                concatList.labelList?.let { list ->
+                concatList?.labelList?.let { list ->
                     it.getLabelAdapter().submitList(list)
                 }
             }
