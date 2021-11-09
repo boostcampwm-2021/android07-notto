d773855622e361b5e9150e46a39355b947eb4b6e
keykat <koin12000@naver.com>
2021-11-09 19:36:04 +0900

    #89 [feat] 반복 시작 다이얼로그 추가

---
+++ app/src/main/java/com/gojol/notto/ui/home/TodoRepeatTimeDialog.kt
@@ -0,0 +1,59 @@
+package com.gojol.notto.ui.home
+
+import android.app.Dialog
+import android.content.Context
+import android.view.View
+import android.view.Window
+import android.view.LayoutInflater
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import com.gojol.notto.R
+import com.gojol.notto.databinding.DialogTodoRepeatTimeBinding
+import java.util.*
+
+
+class TodoRepeatTimeDialog(context: Context) : View(context) {
+
+    private val binding: DialogTodoRepeatTimeBinding = DataBindingUtil.inflate(
+        LayoutInflater.from(context), R.layout.dialog_todo_repeat_time,
+        null,
+        false
+    )
+    private var dialog: Dialog = Dialog(context)
+
+    init {
+
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+        dialog.setContentView(binding.root)
+        dialog.window?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.bg_dialog))
+
+        initClickListener()
+        initCalendar()
+        show()
+    }
+
+    private fun initClickListener() {
+        binding.btnDialogRepeatTimeConfirm.setOnClickListener {
+            confirm()
+        }
+        binding.btnDialogRepeatTimeReject.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun initCalendar() {
+        binding.cvRepeatTime.setSelectedDate(Date(System.currentTimeMillis()))
+    }
+
+    fun show() {
+        dialog.show()
+    }
+
+    private fun confirm() {
+        val selectedDate = binding.cvRepeatTime.selectedDate
+    }
+
+    private fun dismiss() {
+        dialog.dismiss()
+    }
+}
