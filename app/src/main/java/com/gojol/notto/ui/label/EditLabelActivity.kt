75bd151230d6852a72c03dabe43cb77f6c773b5a
Hyesung <comet_0419@naver.com>
2021-11-09 13:46:11 +0900

    #4 [feat] 라벨 편집 레이아웃

---
+++ app/src/main/java/com/gojol/notto/ui/label/EditLabelActivity.kt
@@ -0,0 +1,12 @@
+package com.gojol.notto.ui.label
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.gojol.notto.R
+
+class EditLabelActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_label)
+    }
+}
