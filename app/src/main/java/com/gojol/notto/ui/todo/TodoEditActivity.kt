1f56f31c0f56ad91c51605f5b3f19b6d8858a848
MinjungShin <ydaispaxero@naver.com>
2021-11-09 20:09:31 +0900

    #5 [feat] Todo 편집 화면 구현

---
+++ app/src/main/java/com/gojol/notto/ui/todo/TodoEditActivity.kt
@@ -0,0 +1,36 @@
+package com.gojol.notto.ui.todo
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.gojol.notto.R
+import com.gojol.notto.databinding.ActivityTodoEditBinding
+
+class TodoEditActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityTodoEditBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_todo_edit)
+
+        initAppbar()
+    }
+
+    private fun initAppbar() {
+        binding.tbTodoEdit.setNavigationOnClickListener {
+            // TODO: activity finish
+        }
+
+        binding.tbTodoEdit.setOnMenuItemClickListener {
+            when (it.itemId) {
+                R.id.delete_todo -> {
+                    //TODO todo 삭제
+                    println("delete todo")
+                    true
+                }
+                else -> false
+            }
+        }
+    }
+}
