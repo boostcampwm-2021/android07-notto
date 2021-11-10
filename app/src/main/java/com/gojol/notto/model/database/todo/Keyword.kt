057c948e69004e4289549ac6fccb57fa2f96ed6d
dddooo9 <dlaehdus99@naver.com>
2021-11-11 01:15:05 +0900

    #94 [feat] Keyword Column 추가

--- app/src/main/java/com/gojol/notto/model/database/todo/Keyword.kt
+++ app/src/main/java/com/gojol/notto/model/database/todo/Keyword.kt
@@ -7,5 +7,6 @@ import androidx.room.PrimaryKey
 @Entity
 data class Keyword (
     @ColumnInfo(name = "word") val word: String,
+    @ColumnInfo(name = "parent_todo_id") val parentTodoId: Int,
     @PrimaryKey(autoGenerate = true) var keywordId: Int = 0
 )
