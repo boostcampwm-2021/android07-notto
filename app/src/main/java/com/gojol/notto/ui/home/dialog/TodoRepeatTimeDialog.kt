b20103e32608c24fd73310aed92bbfaf33d2b997
keykat <koin12000@naver.com>
2021-11-10 17:17:51 +0900

    #89 [feat] 반복 시간 다이얼로그 baseDialog에 상속 및 수정

--- app/src/main/java/com/gojol/notto/ui/home/dialog/TodoRepeatTimeDialog.kt
+++ app/src/main/java/com/gojol/notto/ui/home/dialog/TodoRepeatTimeDialog.kt
@@ -1,42 +1,30 @@
 package com.gojol.notto.ui.home.dialog
 
-import android.app.Dialog
 import android.content.Context
-import android.view.View
-import android.view.Window
 import android.view.LayoutInflater
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.gojol.notto.R
 import com.gojol.notto.databinding.DialogTodoRepeatTimeBinding
 import java.util.*
 
 
-class TodoRepeatTimeDialog(context: Context) : View(context) {
-
+class TodoRepeatTimeDialog(context: Context) : TodoBaseDialogImpl(context) {
     private val binding: DialogTodoRepeatTimeBinding = DataBindingUtil.inflate(
         LayoutInflater.from(context), R.layout.dialog_todo_repeat_time,
         null,
         false
     )
-    private var dialog: Dialog = Dialog(context)
 
     init {
-
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(binding.root)
-        dialog.window?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.bg_dialog))
-
         initClickListener()
         initCalendar()
-        show()
     }
 
     private fun initClickListener() {
-        binding.btnDialogRepeatTimeConfirm.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseConfirm.setOnClickListener {
             confirm()
         }
-        binding.btnDialogRepeatTimeReject.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseReject.setOnClickListener {
             dismiss()
         }
     }
@@ -45,15 +33,8 @@ class TodoRepeatTimeDialog(context: Context) : View(context) {
         binding.cvRepeatTime.setSelectedDate(Date(System.currentTimeMillis()))
     }
 
-    fun show() {
-        dialog.show()
-    }
-
-    private fun confirm() {
+    override fun confirm() {
         val selectedDate = binding.cvRepeatTime.selectedDate
-    }
-
-    private fun dismiss() {
-        dialog.dismiss()
+        super.confirm()
     }
 }
