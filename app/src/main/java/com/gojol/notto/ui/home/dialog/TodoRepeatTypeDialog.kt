0f4d1e3e6a7f5b7009b8d2d4b7db1b1fb40c9893
keykat <koin12000@naver.com>
2021-11-10 17:18:02 +0900

    #89 [feat] 반복 유형 다이얼로그 baseDialog에 상속 및 수정

--- app/src/main/java/com/gojol/notto/ui/home/dialog/TodoRepeatTypeDialog.kt
+++ app/src/main/java/com/gojol/notto/ui/home/dialog/TodoRepeatTypeDialog.kt
@@ -1,53 +1,29 @@
 package com.gojol.notto.ui.home.dialog
 
-import android.app.Dialog
 import android.content.Context
-import android.view.View
-import android.view.Window
 import android.view.LayoutInflater
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.gojol.notto.R
 import com.gojol.notto.databinding.DialogTodoRepeatTypeBinding
 
 
-class TodoRepeatTypeDialog(context: Context) : View(context) {
-
+class TodoRepeatTypeDialog(context: Context) : TodoBaseDialogImpl(context) {
     private val binding: DialogTodoRepeatTypeBinding = DataBindingUtil.inflate(
         LayoutInflater.from(context), R.layout.dialog_todo_repeat_type,
         null,
         false
     )
-    private var dialog: Dialog = Dialog(context)
 
     init {
-
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(binding.root)
-        dialog.window?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.bg_dialog))
-
         initClickListener()
-        show()
     }
 
     private fun initClickListener() {
-        binding.btnDialogRepeatTypeConfirm.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseConfirm.setOnClickListener {
             confirm()
         }
-        binding.btnDialogRepeatTypeReject.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseReject.setOnClickListener {
             dismiss()
         }
-    }
-
-    fun show() {
-        dialog.show()
-    }
-
-    private fun confirm() {
-
-    }
-
-    private fun dismiss() {
-        dialog.dismiss()
     }
 }
