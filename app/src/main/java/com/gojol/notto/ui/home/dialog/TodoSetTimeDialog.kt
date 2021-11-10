30c37fc0859f549fb70937b4f7cec7cf988ff6f4
keykat <koin12000@naver.com>
2021-11-10 17:18:20 +0900

    #89 [feat] 시간 설정 다이얼로그 baseDialog에 상속 및 수정

--- app/src/main/java/com/gojol/notto/ui/home/dialog/TodoSetTimeDialog.kt
+++ app/src/main/java/com/gojol/notto/ui/home/dialog/TodoSetTimeDialog.kt
@@ -1,50 +1,36 @@
 package com.gojol.notto.ui.home.dialog
 
-import android.app.Dialog
 import android.content.Context
 import android.os.Build
-import android.view.View
-import android.view.Window
 import android.view.LayoutInflater
-import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.gojol.notto.R
 import com.gojol.notto.databinding.DialogTodoSetTimeBinding
 
 
-class TodoSetTimeDialog(context: Context) : View(context) {
+class TodoSetTimeDialog(context: Context) : TodoBaseDialogImpl(context) {
 
     private val binding: DialogTodoSetTimeBinding = DataBindingUtil.inflate(
         LayoutInflater.from(context), R.layout.dialog_todo_set_time,
         null,
         false
     )
-    private var dialog: Dialog = Dialog(context)
 
     init {
-
-        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
-        dialog.setContentView(binding.root)
-        dialog.window?.setBackgroundDrawable(ContextCompat.getDrawable(context, R.drawable.bg_dialog))
-
         initClickListener()
         show()
     }
 
     private fun initClickListener() {
-        binding.btnDialogSetTimeConfirm.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseConfirm.setOnClickListener {
             confirm()
         }
-        binding.btnDialogSetTimeReject.setOnClickListener {
+        binding.bvDialogDeletion.btnDialogBaseReject.setOnClickListener {
             dismiss()
         }
     }
 
-    fun show() {
-        dialog.show()
-    }
-
-    private fun confirm() {
+    override fun confirm() {
         if (Build.VERSION.SDK_INT >= 23) {
             val hour = binding.tpSetTime.hour
             val minute = binding.tpSetTime.minute
@@ -52,9 +38,6 @@ class TodoSetTimeDialog(context: Context) : View(context) {
             val hour = binding.tpSetTime.currentHour
             val minute = binding.tpSetTime.currentMinute
         }
-    }
-
-    private fun dismiss() {
-        dialog.dismiss()
+        super.confirm()
     }
 }
