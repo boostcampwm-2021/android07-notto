833daebf130f6182cb361e1806c499f247789a52
dddooo9 <dlaehdus99@naver.com>
2021-11-11 01:22:14 +0900

    #94 [feat] Database에 DateState, Keyword Entity 정의

--- app/src/main/java/com/gojol/notto/model/database/TodoLabelDatabase.kt
+++ app/src/main/java/com/gojol/notto/model/database/TodoLabelDatabase.kt
@@ -3,11 +3,13 @@ package com.gojol.notto.model.database
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.gojol.notto.model.database.label.Label
+import com.gojol.notto.model.database.todo.DateState
+import com.gojol.notto.model.database.todo.Keyword
 import com.gojol.notto.model.database.todo.Todo
 import com.gojol.notto.model.database.todolabel.TodoLabelCrossRef
 import com.gojol.notto.model.database.todolabel.TodoLabelDao
 
-@Database(entities = [Todo::class, Label::class, TodoLabelCrossRef::class], version = 1)
+@Database(entities = [Todo::class, Label::class, DateState::class, Keyword::class, TodoLabelCrossRef::class], version = 1)
 abstract class TodoLabelDatabase : RoomDatabase() {
 
     abstract fun todoLabelDao(): TodoLabelDao
