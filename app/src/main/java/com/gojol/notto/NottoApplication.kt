058380383444855ee663537f4e6d3e89c3460c88
dddooo9 <dlaehdus99@naver.com>
2021-11-15 20:17:32 +0900

    [feat] stetho 적용

--- app/src/main/java/com/gojol/notto/NottoApplication.kt
+++ app/src/main/java/com/gojol/notto/NottoApplication.kt
@@ -1,8 +1,14 @@
 package com.gojol.notto
 
 import android.app.Application
+import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class NottoApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+        Stetho.initializeWithDefaults(this)
+    }
 }
