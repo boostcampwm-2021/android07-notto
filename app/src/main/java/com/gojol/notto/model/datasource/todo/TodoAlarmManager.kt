dfe39c21554dd56a7bdeae69a7fbdbb61fa6c5f9
keykat <koin12000@naver.com>
2021-11-15 22:04:34 +0900

    #21 [feat] AlarmManager 인터페이스 구현

---
+++ app/src/main/java/com/gojol/notto/model/datasource/todo/TodoAlarmManager.kt
@@ -0,0 +1,12 @@
+package com.gojol.notto.model.datasource.todo
+
+import com.gojol.notto.model.database.todo.Todo
+
+interface TodoAlarmManager {
+
+    fun addAlarm(todo: Todo)
+
+    fun updateAlarm(todo: Todo)
+
+    fun deleteAlarm(todo: Todo)
+}
