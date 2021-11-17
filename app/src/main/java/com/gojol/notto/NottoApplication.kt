bafc85bb4d29c4097a493053e3757647e89e1cce
keykat <koin12000@naver.com>
2021-11-18 03:46:28 +0900

    #21 [feat] Work Manager Factory 추가

--- app/src/main/java/com/gojol/notto/NottoApplication.kt
+++ app/src/main/java/com/gojol/notto/NottoApplication.kt
@@ -1,14 +1,26 @@
 package com.gojol.notto
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import com.facebook.stetho.Stetho
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class NottoApplication: Application() {
+class NottoApplication : Application(), Configuration.Provider {
+
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
+
     override fun onCreate() {
         super.onCreate()
 
         Stetho.initializeWithDefaults(this)
     }
+
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+        .setWorkerFactory(workerFactory)
+        .build()
 }
