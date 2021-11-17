c53a99f93e66e69b05951720ac87c6aa12f44797
keykat <koin12000@naver.com>
2021-11-18 03:51:37 +0900

    #21 [feat] Notification 터치 이벤트 구현
    - 이런 로직이면 Work Manager를 쓰는 이유가 있는가?

--- app/src/main/java/com/gojol/notto/util/TodoSuccessCheckBroadcastReceiver.kt
+++ app/src/main/java/com/gojol/notto/util/TodoSuccessCheckBroadcastReceiver.kt
@@ -2,9 +2,17 @@ package com.gojol.notto.util
 
 import android.content.Context
 import android.content.Intent
+import androidx.work.OneTimeWorkRequestBuilder
+import androidx.work.WorkManager
+import androidx.work.workDataOf
 import com.gojol.notto.model.database.todo.Todo
 import com.gojol.notto.model.datasource.todo.TodoLabelRepository
 import javax.inject.Inject
+import android.app.NotificationManager
+
+const val SUCCESS_INTENT_ID = 1000000001
+const val FAIL_INTENT_ID = 1000000002
+const val NOTIFICATION_TODO = "notificationTodo"
 
 class TodoSuccessCheckBroadcastReceiver : HiltBroadcastReceiver() {
 
@@ -13,19 +21,38 @@ class TodoSuccessCheckBroadcastReceiver : HiltBroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
         super.onReceive(context, intent)
-        if(intent.getStringExtra(ACTION_STATE) == ACTION_SUCCESS) {
-            intent.getSerializableExtra(ACTION_BUNDLE)?.let {
-                val todo = it as Todo
-                // TODO : todo에서 dailyTodo 찾아서 copy하고 success 체크하고 update하기
-                //  repository.updateDailyTodo(todo)
-                //  coroutine이든 work manager든..
+
+        if (intent.getStringExtra(ACTION_STATE) == ACTION_SUCCESS) {
+            intent.getBundleExtra(ACTION_BUNDLE)?.let {
+                val todo = it.getSerializable(ACTION_TODO_DATA) as Todo
+                val data = workDataOf(NOTIFICATION_TODO to todo.todoId)
+
+                val workManager = WorkManager.getInstance(context)
+                val workRequest =
+                    OneTimeWorkRequestBuilder<SuccessButtonWorker>()
+                        //.setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                        .setInputData(data)
+                        .build()
+                workManager.enqueue(workRequest)
+
+                val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.cancel(todo.todoId)
             }
-        } else if(intent.getStringExtra(ACTION_STATE) == ACTION_FAIL) {
-            intent.getSerializableExtra(ACTION_BUNDLE)?.let {
-                val todo = it as Todo
-                // TODO : todo에서 dailyTodo 찾아서 copy하고 fail 체크하고 update하기
-                //  repository.updateDailyTodo(todo)
-                //  coroutine이든 work manager든..
+        } else if (intent.getStringExtra(ACTION_STATE) == ACTION_FAIL) {
+            intent.getBundleExtra(ACTION_BUNDLE)?.let {
+                val todo = it.getSerializable(ACTION_TODO_DATA) as Todo
+                val data = workDataOf(NOTIFICATION_TODO to todo.todoId)
+
+                val workManager = WorkManager.getInstance(context)
+                val workRequest =
+                    OneTimeWorkRequestBuilder<FailButtonWorker>()
+                        //.setExpedited(OutOfQuotaPolicy.RUN_AS_NON_EXPEDITED_WORK_REQUEST)
+                        .setInputData(data)
+                        .build()
+                workManager.enqueue(workRequest)
+
+                val manager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+                manager.cancel(todo.todoId)
             }
         }
     }
