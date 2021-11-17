73557b916e8c87418d98e73ffb115456b9c93db8
dddooo9 <dlaehdus99@naver.com>
2021-11-17 16:34:16 +0900

    #11 [fix] CalendarViewModel 리팩토링

--- app/src/main/java/com/gojol/notto/ui/home/CalendarViewModel.kt
+++ app/src/main/java/com/gojol/notto/ui/home/CalendarViewModel.kt
@@ -1,6 +1,5 @@
 package com.gojol.notto.ui.home
 
-import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
@@ -12,8 +11,6 @@ import com.gojol.notto.model.datasource.todo.TodoLabelRepository
 import com.gojol.notto.util.getDate
 import com.gojol.notto.util.getDayOfWeek
 import com.gojol.notto.util.getLastDayOfMonth
-import com.gojol.notto.util.getMonth
-import com.gojol.notto.util.getYear
 import com.gojol.notto.util.toYearMonthDate
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -25,95 +22,89 @@ class CalendarViewModel @Inject constructor(
     private val repository: TodoLabelRepository
 ) : ViewModel() {
 
-    private var _year = 0
-    private var _month = 0
-    private val _monthStartDate = MutableLiveData<String>()
-    private val _monthLastDate = MutableLiveData<String>()
-    private val _monthDateList = MutableLiveData<List<Int>>()
-    private val _monthlyDailyTodos = MutableLiveData<List<DailyTodo>>()
+    private var currentCalendarYear = 0
+    private var currentCalendarMonth = 0
+    private var monthStartDate: String = ""
+    private var monthLastDate: String = ""
+    private var monthDateList = emptyList<Int>()
+    private var monthlyDailyTodos = emptyList<DailyTodo>()
 
     private val _monthlyAchievement = MutableLiveData<List<DateWithCountAndSelect>>()
     val monthlyAchievement: LiveData<List<DateWithCountAndSelect>> = _monthlyAchievement
 
-    private val _selectedDate = MutableLiveData<Int>()
-    val selectedDate: LiveData<Int> = _selectedDate
-
     fun setMonthDate(year: Int, month: Int) {
-        _year = year
-        _month = month
+        currentCalendarYear = year
+        currentCalendarMonth = month
 
-        val monthStartDate = Calendar.getInstance().apply {
+        val startDate = Calendar.getInstance().apply {
             set(year, month, 1)
         }
-        _monthStartDate.value = monthStartDate.toYearMonthDate()
+        monthStartDate = startDate.toYearMonthDate()
 
-        val monthLastDate = Calendar.getInstance().apply {
-            set(year, month, monthStartDate.getLastDayOfMonth())
+        val lastDate = Calendar.getInstance().apply {
+            set(year, month, startDate.getLastDayOfMonth())
         }
-        _monthLastDate.value = monthLastDate.toYearMonthDate()
+        monthLastDate = lastDate.toYearMonthDate()
 
-        val dateList = (monthStartDate.getDate()..monthLastDate.getDate()).toList()
-        val dayOfWeek = monthStartDate.getDayOfWeek() - 1
+        val dateList = (startDate.getDate()..lastDate.getDate()).toList()
+        val dayOfWeek = startDate.getDayOfWeek() - 1
         val prefixDateList = (0 until dayOfWeek).map { 0 }
-        _monthDateList.value = prefixDateList + dateList
+        monthDateList = prefixDateList + dateList
     }
 
     fun setMonthlyDailyTodos() {
         viewModelScope.launch {
-            _monthStartDate.value?.let { startDate ->
-                _monthLastDate.value?.let { lastDate ->
-                    launch {
-                        _monthlyDailyTodos.value = repository.getAllDailyTodos().filter {
-                            it.date > startDate && it.date < lastDate
-                        }
-                    }.join()
-
-                    setMonthlyAchievement()
+            launch {
+                monthlyDailyTodos = repository.getAllDailyTodos().filter {
+                    it.date > monthStartDate && it.date < monthLastDate
                 }
-            }
+            }.join()
+
+            setMonthlyAchievement()
         }
     }
 
     fun setMonthlyAchievement() {
-        _monthlyAchievement.value = _monthDateList.value?.map { date ->
-            val todayDailyTodos = _monthlyDailyTodos.value
-                ?.filter { it.date.takeLast(2).toInt() == date }
+        _monthlyAchievement.value = monthDateList.map { date ->
+            val todayDailyTodos = monthlyDailyTodos
+                .filter { it.date.takeLast(2).toInt() == date }
 
-            val successCount = todayDailyTodos?.count { it.todoState == TodoState.SUCCESS } ?: 0
+            DateWithCountAndSelect(date, getSuccessLevel(todayDailyTodos), getSuccess(date))
+        }
+    }
 
-            val totalCount = todayDailyTodos?.size ?: 0
+    private fun getSuccessLevel(todayDailyTodos: List<DailyTodo>): Int {
+        val successCount = todayDailyTodos.count { it.todoState == TodoState.SUCCESS }
+        val totalCount = todayDailyTodos.size
 
-            val successRate = if (totalCount == 0) {
-                0.toFloat()
-            } else {
-                successCount.toFloat() / totalCount.toFloat()
-            }
+        val successRate = if (totalCount == 0) {
+            0.toFloat()
+        } else {
+            successCount.toFloat() / totalCount.toFloat()
+        }
 
-            var successLevel = when {
-                successRate <= 0.25 -> 1
-                successRate <= 0.5 -> 2
-                successRate <= 0.75 -> 3
-                successRate < 1 -> 4
-                else -> 5
-            }
-            if (totalCount == 0 || successCount == 0) {
-                successLevel = 0
-            }
+        var successLevel = when {
+            successRate <= 0.25 -> 1
+            successRate <= 0.5 -> 2
+            successRate <= 0.75 -> 3
+            successRate < 1 -> 4
+            else -> 5
+        }
 
-            val select =
-                if (CalendarFragment.selectedYear == _year &&
-                    CalendarFragment.selectedMonth == _month
-                ) {
-                    date == CalendarFragment.selectedDate ?: 1
-                } else {
-                    false
-                }
+        if (totalCount == 0 || successCount == 0) {
+            successLevel = 0
+        }
 
-            DateWithCountAndSelect(
-                date,
-                successLevel,
-                select
-            )
+        return successLevel
+    }
+
+    private fun getSuccess(date: Int): Boolean {
+        return if (CalendarFragment.selectedYear == currentCalendarYear &&
+            CalendarFragment.selectedMonth == currentCalendarMonth
+        ) {
+            date == CalendarFragment.selectedDate ?: 1
+        } else {
+            false
         }
     }
 }
