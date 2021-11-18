e65564f7f4a862d2fe72227cbb16b96bafece2c0
keykat <koin12000@naver.com>
2021-11-18 18:33:09 +0900

    #21 [feat] 다음날의 dailyTodo 추가 Worker 생성

--- app/src/main/java/com/gojol/notto/util/AddDailyTodoWorker.kt
+++ app/src/main/java/com/gojol/notto/util/AddDailyTodoWorker.kt
@@ -4,39 +4,36 @@ import android.content.Context
 import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
-import com.gojol.notto.common.TodoState
 import com.gojol.notto.model.datasource.todo.TodoLabelRepository
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
-import kotlinx.coroutines.delay
 import java.lang.Exception
 import java.util.*
 
+const val UPDATE_TOMORROW_DAILY_TODO = "com.notto.util.AddDailyTodoWorker"
+
 @HiltWorker
-class TestWorker @AssistedInject constructor(
-    @Assisted private val context: Context,
+class AddDailyTodoWorker @AssistedInject constructor(
+    @Assisted context: Context,
     @Assisted params: WorkerParameters,
     private val repository: TodoLabelRepository
 ) : CoroutineWorker(context, params) {
     override suspend fun doWork(): Result {
         return try {
-            recreateAlarm()
+            addTomorrowDailyTodosAlarm()
             Result.success()
         } catch (e: Exception) {
             Result.failure()
         }
     }
 
-    private suspend fun recreateAlarm() {
-        val currentDate = Date(System.currentTimeMillis()).getDateString()
-        repository.getTodosWithTodayDailyTodos(currentDate).forEach {
-            val todo = it.todo
-            val dailyTodo = it.todayDailyTodo
-            if (todo.hasAlarm && dailyTodo != null && dailyTodo.todoState != TodoState.SUCCESS) {
-                println(todo)
-                repository.addAlarm(todo)
-            }
-            delay(1000)
+    private suspend fun addTomorrowDailyTodosAlarm() {
+        val calendar = Calendar.getInstance()
+        if (calendar.get(Calendar.HOUR_OF_DAY) >= 23) {
+            calendar.add(Calendar.DAY_OF_YEAR, 1)
+            val tomorrowDailyTodos =
+                repository.getTodosWithTodayDailyTodos(calendar.time.getDateString())
+            println(tomorrowDailyTodos)
         }
     }
 }
