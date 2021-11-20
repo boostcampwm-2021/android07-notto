c1634d7b27ae06ee2cc57ccf33402e7cbed2bbb6
keykat <koin12000@naver.com>
2021-11-21 07:02:24 +0900

    #152 [feat] 성공 후 알림 종료 조건 추가

--- app/src/main/java/com/gojol/notto/model/datasource/todo/TodoAlarmManagerImpl.kt
+++ app/src/main/java/com/gojol/notto/model/datasource/todo/TodoAlarmManagerImpl.kt
@@ -11,31 +11,32 @@ import com.gojol.notto.util.getDate
 import com.gojol.notto.util.getTotalTimeString
 import javax.inject.Inject
 import android.os.Bundle
+import com.gojol.notto.common.TodoState
+import com.gojol.notto.model.database.todo.DailyTodo
+import com.google.gson.Gson
 
 const val ALARM_EXTRA_TODO = "alarmExtraTodo"
-const val ALARM_BUNDLE_TODO = "alarmBundleTodo"
 
 class TodoAlarmManagerImpl @Inject constructor(
     private val context: Context,
     private val alarmManager: AlarmManager
 ) : TodoAlarmManager {
 
+    private val gson: Gson = Gson()
+
     @SuppressLint("UnspecifiedImmutableFlag")
     override fun addAlarm(todo: Todo) {
         if (!todo.hasAlarm) return
 
-        val intent = Intent(context, TodoPushBroadcastReceiver::class.java)
-        val bundle = Bundle()
-        bundle.putSerializable(ALARM_EXTRA_TODO, todo)
-        intent.putExtra(ALARM_BUNDLE_TODO, bundle)
+        val intent = Intent(context, TodoPushBroadcastReceiver::class.java).apply {
+            putExtra(ALARM_EXTRA_TODO, gson.toJson(todo))
+        }
 
         val pendingIntent = PendingIntent.getBroadcast(
             context, todo.todoId, intent,
             PendingIntent.FLAG_UPDATE_CURRENT
         )
 
-        // TODO : Extension에 비슷한 기능을 하는 중복된 코드가 많은 것 같다. pattern을 넣어서 선언할 수 있도록 수정해보자.
-        //  또한 패턴을 const 등으로 정리하고 저장해서 가져다 쓰는 식으로 하면 좋을 것 같다.
         val fullDate = ("${todo.startDate} ${todo.startTime}").getDate("yyyyMMdd a hh:mm")
         fullDate?.let { it ->
             val repeatInterval: Long = todo.periodTime.time.toInt() * 60 * 1000L
@@ -46,7 +47,6 @@ class TodoAlarmManagerImpl @Inject constructor(
                 triggerTime, repeatInterval,
                 pendingIntent
             )
-            println("register : " + System.currentTimeMillis().getTotalTimeString())
         }
     }
 
@@ -54,10 +54,19 @@ class TodoAlarmManagerImpl @Inject constructor(
     override fun deleteAlarm(todo: Todo) {
         val todoTime = ("${todo.startDate} ${todo.endTime}").getDate("yyyyMMdd a hh:mm") ?: return
         val endTime = todoTime.time - todo.periodTime.time.toInt() * 60 * 1000
-        println("${todo.startDate} ${todo.endTime}")
-        println(endTime.getTotalTimeString())
-        println(System.currentTimeMillis().getTotalTimeString())
+
         if (!todo.isRepeated || endTime < System.currentTimeMillis()) {
+            val intent = Intent(context, TodoPushBroadcastReceiver::class.java)
+            val pendingIntent = PendingIntent.getBroadcast(
+                context, todo.todoId, intent, PendingIntent.FLAG_UPDATE_CURRENT
+            )
+            alarmManager.cancel(pendingIntent)
+        }
+    }
+
+    @SuppressLint("UnspecifiedImmutableFlag")
+    override fun deleteAlarm(todo: Todo, todoState: TodoState) {
+        if(todoState == TodoState.SUCCESS) {
             val intent = Intent(context, TodoPushBroadcastReceiver::class.java)
             val pendingIntent = PendingIntent.getBroadcast(
                 context, todo.todoId, intent, PendingIntent.FLAG_UPDATE_CURRENT
