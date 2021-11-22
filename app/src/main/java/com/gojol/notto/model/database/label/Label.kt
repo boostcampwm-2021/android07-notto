89a52e1cc668453c33347f6de2c4dbcc50a9e177
Hyesung <comet_0419@naver.com>
2021-11-22 21:54:13 +0900

    [fix] Label.kt에 Parcelize 적용

--- app/src/main/java/com/gojol/notto/model/database/label/Label.kt
+++ app/src/main/java/com/gojol/notto/model/database/label/Label.kt
@@ -1,40 +1,15 @@
 package com.gojol.notto.model.database.label
 
-import android.os.Parcel
 import android.os.Parcelable
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
 
 @Entity
+@Parcelize
 data class Label(
     @ColumnInfo(name = "order") val order: Int,
     @ColumnInfo(name = "name") val name: String,
     @PrimaryKey(autoGenerate = true) var labelId: Int = 0
-) : Parcelable {
-    constructor(parcel: Parcel) : this(
-        parcel.readInt(),
-        parcel.readString()!!,
-        parcel.readInt()
-    )
-
-    override fun writeToParcel(parcel: Parcel, flags: Int) {
-        parcel.writeInt(order)
-        parcel.writeString(name)
-        parcel.writeInt(labelId)
-    }
-
-    override fun describeContents(): Int {
-        return 0
-    }
-
-    companion object CREATOR : Parcelable.Creator<Label> {
-        override fun createFromParcel(parcel: Parcel): Label {
-            return Label(parcel)
-        }
-
-        override fun newArray(size: Int): Array<Label?> {
-            return arrayOfNulls(size)
-        }
-    }
-}
+) : Parcelable
