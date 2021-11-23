54fca5727d26a4ccc73959c2b31d4c4226b69f56
keykat <koin12000@naver.com>
2021-11-23 18:31:24 +0900

    #135 [feat] const 변수 이동

--- app/src/main/java/com/gojol/notto/common/Constants.kt
+++ app/src/main/java/com/gojol/notto/common/Constants.kt
@@ -12,3 +12,14 @@ const val ALARM_EXTRA_TODO = "alarmExtraTodo"
 const val SUCCESS_INTENT_ID = 1000000001
 const val FAIL_INTENT_ID = 1000000002
 const val NOTIFICATION_TODO = "notificationTodo"
+
+// dialog
+const val SET_TIME_DATA = "setTimeData"
+const val SET_TIME_START = "timeStart"
+const val SET_TIME_FINISH = "timeFinish"
+const val REPEAT_TIME_DATA = "repeatTimeData"
+const val REPEAT_TIME = "repeatTime"
+const val REPEAT_TYPE_DATA = "repeatTypeValue"
+const val REPEAT_TYPE = "repeatType"
+const val TIME_REPEAT_DATA = "timeRepeatData"
+const val TIME_REPEAT = "timeRepeat"
