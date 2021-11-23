b8a2edc7a4cced591961f967b2acca6846e11fe6
keykat <koin12000@naver.com>
2021-11-23 18:39:47 +0900

    #135 [fix] repeatType dialog 변경

---
+++ app/src/main/java/com/gojol/notto/ui/todo/dialog/RepeatTypeDialog.kt
@@ -0,0 +1,66 @@
+package com.gojol.notto.ui.todo.dialog
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
+import com.gojol.notto.R
+import com.gojol.notto.common.REPEAT_TYPE_DATA
+import com.gojol.notto.common.TodoDeleteType
+import com.gojol.notto.databinding.DialogDeletionBinding
+import com.gojol.notto.databinding.DialogTodoRepeatTypeBinding
+import com.gojol.notto.model.data.RepeatType
+import com.gojol.notto.ui.todo.dialog.util.DeletionDialogViewModel
+import com.gojol.notto.ui.todo.dialog.util.RepeatTypeDialogViewModel
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class RepeatTypeDialog : BaseDialog<DialogTodoRepeatTypeBinding, RepeatTypeDialogViewModel>() {
+
+    override val viewModel: RepeatTypeDialogViewModel by viewModels()
+    override var layoutId: Int = R.layout.dialog_todo_repeat_type
+
+    var repeatTypeCallback: ((RepeatType) -> Unit)? = null
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.viewmodel = viewModel
+        repeatTypeCallback?.let { viewModel.setRepeatTypeCallback(it) }
+        initRepeatType()
+    }
+
+    private fun initRepeatType() {
+        arguments?.let { arg ->
+            arg.getSerializable(REPEAT_TYPE_DATA)?.let {
+                viewModel.setRepeatType(it as RepeatType)
+                arg.remove(REPEAT_TYPE_DATA)
+            }
+        }
+    }
+
+    override fun confirmClick() {
+        viewModel.repeatType.value?.let{
+            viewModel.repeatTypeCallback.value?.let { callback ->
+                callback(it)
+            }
+        }
+        this.dismiss()
+    }
+
+    override fun dismissClick() {
+        this.dismiss()
+    }
+
+    override fun initObserver() {}
+
+    companion object {
+        fun newInstance(bundle: Bundle, callback: (RepeatType) -> Unit): RepeatTypeDialog {
+            return RepeatTypeDialog().apply {
+                arguments = bundle
+                this.repeatTypeCallback = callback
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/gojol/notto/ui/todo/dialog/util/RepeatTypeDialogViewModel.kt
@@ -0,0 +1,22 @@
+package com.gojol.notto.ui.todo.dialog.util
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import com.gojol.notto.model.data.RepeatType
+
+class RepeatTypeDialogViewModel : DialogViewModel() {
+
+    private val _repeatType = MutableLiveData<RepeatType>()
+    val repeatType: LiveData<RepeatType> = _repeatType
+
+    private val _repeatTypeCallback = MutableLiveData<(RepeatType) -> Unit>()
+    val repeatTypeCallback: LiveData<(RepeatType) -> Unit> = _repeatTypeCallback
+
+    fun setRepeatType(type: RepeatType) {
+        _repeatType.value = type
+    }
+
+    fun setRepeatTypeCallback(callback: (RepeatType) -> Unit) {
+        _repeatTypeCallback.value = callback
+    }
+}
