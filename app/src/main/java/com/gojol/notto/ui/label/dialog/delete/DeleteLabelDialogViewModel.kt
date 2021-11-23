affeae9e4f9503e24669f404c38862c1da0aa788
keykat <koin12000@naver.com>
2021-11-23 19:54:54 +0900

    #135 [fix] DeleteLabelDialogViewModel.kt 변경

--- app/src/main/java/com/gojol/notto/ui/label/dialog/delete/DeleteLabelDialogViewModel.kt
+++ app/src/main/java/com/gojol/notto/ui/label/dialog/delete/DeleteLabelDialogViewModel.kt
@@ -2,30 +2,22 @@ package com.gojol.notto.ui.label.dialog.delete
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.gojol.notto.model.database.label.Label
 import com.gojol.notto.model.datasource.todo.TodoLabelRepository
+import com.gojol.notto.ui.DialogViewModel
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class DeleteLabelDialogViewModel @Inject constructor(private val repository: TodoLabelRepository) :
-    ViewModel() {
+    DialogViewModel() {
 
     private val _label = MutableLiveData<Label>()
     val label: LiveData<Label> = _label
 
-    private val _close = MutableLiveData(false)
-    val close: LiveData<Boolean> = _close
-
-    fun clickCancel() {
-        _close.value = true
-    }
-
     fun clickOkay() {
-        _close.value = true
         label.value ?: return
 
         viewModelScope.launch {
